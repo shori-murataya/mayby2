1f2cb3298eac4dfaee325a4d902d20110d1ee116
shori-murata <mura.shori.king.king@gamil.com>
2020-07-10 11:28:58 +0900

    add postspages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
   post 'posts/create' => "posts#create"
+  post 'posts/:id/destroy' => "posts#destroy" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "home#top"
   get "about" => "home#about"
