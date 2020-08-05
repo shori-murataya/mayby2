9a80711cc3e07687d347c8891eaab5095efdf351
shori-murata <mura.shori.king.king@gamil.com>
2020-08-06 02:24:41 +0900

    Tried Improve My Routes Codes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,38 +1,29 @@
 class PostsController < ApplicationController
-
-  def show
-    @post = Post.find_by(id: params[:id])
-    @posts = Post.where(id: params[:id])
-    @user = User.find_by(id: @post.user_id)
-    @comment = Comment.new
-    @comments = Comment.where(post_id: params[:id]).page(params[:page]).per(2).order(created_at: :desc)
-    @likes = Like.where(post_id: params[:id]).order(created_at: :desc).limit(1)
-    @like = Like.where(post_id:params[:id])
-  end 
-
+  
   def index
     @q = Post.ransack(params[:q])
     @q.build_sort if @q.sorts.empty?
     @posts = @q.result.page(params[:page]).per(2).order(created_at: :desc)
   end
 
+  def show
+    @post = Post.find_by(id: params[:id])
+    @posts = Post.where(id: params[:id])
+    @comment = Comment.new
+    @comments = Comment.where(post_id: params[:id]).page(params[:page]).per(2).order(created_at: :desc)
+  end 
+
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(
-      title: params[:title],
-      howto: params[:howto],
-      user_id: current_user.id,
-      num_of_people: params[:num_of_people],
-      play_style: params[:play_style]
-      )
-    if @post.save
-      redirect_to("/posts/index")
-    else
-      render("posts/new")
-    end
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to posts_path
+      else
+        render "new"
+      end
   end
 
   def edit
@@ -41,25 +32,21 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find_by(id: params[:id])
-    @post.title = params[:title]
-    @post.howto = params[:howto]
-    @post.num_of_people = params[:num_of_people]
-    @post.play_style = params[:play_style]
-    if @post.save
-      flash[:notice] = "編集完了"
-      redirect_to("/posts/index")
-    else
-      flash[:notice] = "編集失敗"
-      render("posts/edit")
+    if @post.user_id == current_user.id
+      @post.update(post_params)
+      redirect_to posts_path 
     end
   end
 
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
-    redirect_to("/posts/index")
+    redirect_to posts_path
   end
 
-
+private
+def post_params
+  params.require(:post).permit(:title, :howto, :num_of_people, :play_style, :user_id)
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   post 'comments/:post_id/create' => "comments#create"
   post 'comments/:comment_id/destroy' => "comments#destroy"
   post 'comments/:comment_id/user_come_destroy/' => "comments#user_come_destroy"
@@ -8,18 +9,10 @@ Rails.application.routes.draw do
   post 'likes/:post_id/destroy' => "likes#destroy"
   get 'likes/:post_id/index' => "likes#index"
   
-  get 'users/:id/show' => "users#show"
-  get 'users/index' => "users#index"
+  resources :users, :posts
   
-  get 'posts/:id/show' => "posts#show"
-  get 'posts/index' => "posts#index"
-  get 'posts/new' => "posts#new"
-  post 'posts/create' => "posts#create"
-  post 'posts/:id/destroy' => "posts#destroy" 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root to: "home#top"
-  get "about" => "home#about"
-  get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/update" => "posts#update"
+  get "/about", to: "home#about"
   
 end
