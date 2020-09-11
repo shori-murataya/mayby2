2936c289454d66ab0199a3e1228aaaa4c040091f
shori-murata <mura.shori.king.king@gamil.com>
2020-09-11 23:35:26 +0900

    ???

--- app/assets/javascripts/accordion_search.js
+++ app/assets/javascripts/accordion_search.js
@@ -1,11 +1,11 @@
 $(document).on('turbolinks:load', function(){
   $('.js_open').on('click',function(){
-    let $info = $(this).next('.js_search-box');
-    if($info.hasClass('open')){
-      $info.removeClass('open').slideUp(200);
+    let info = $(this).next('.js_search-box');
+    if(info.hasClass('open')){
+      info.removeClass('open').slideUp(200);
       $(this).find('.check').text('+');
     }else{
-      $info.addClass('open').slideDown(200);
+      info.addClass('open').slideDown(200);
       $(this).find('.check').text('-');
     }
   });
